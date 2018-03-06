201e1f8a372ed93774ebe265f9a4351d08da8e11
Nambisan <akn36d@mst.edu>
2018-03-05 23:37:31 -0600

    Finished draft of u-net model

--- my_models_keras.py
+++ my_models_keras.py
@@ -21,7 +21,7 @@ class epinet_model1_cnn():
         self.img_channels - img_channels
         
     # Ref: http://lmb.informatik.uni-freiburg.de/people/ronneber/u-net/
-    def model(self): 
+    def get_model(self): 
         
         ############################ CONTRACTING PATH #########################
         # First conv section of contracting path 
@@ -152,6 +152,103 @@ class epinet_model1_cnn():
                        activation='relu',
                        padding='same',
                        kernel_initializer='he_normal')(merge6)
-        covn6 = 
+        covn6 = Conv2D(filters=512,
+                       kernel_size=2,
+                       activation='relu',
+                       padding='same',
+                       kernel_initializer='he_normal')(conv6)
+
+
+        # ---------------------------------------------------------------------
+        # Do Upsampling 
+        up7 = Conv2D(filters=256,
+                     kernel_size=2,
+                     activation='relu',
+                     padding='same',
+                     kernel_initializer='he_normal')(UpSampling2D(size=(2,2))(conv6))
         
         
+        # ---------------------------------------------------------------------
+        # Second conv and merge in expanding path
+        merge7 = merge([conv3,up7],
+                       mode='concat',
+                       concat_axis=3)
+        conv7 = Conv2D(filters=256,
+                       kernel_size=2,
+                       activation='relu',
+                       padding='same',
+                       kernel_initializer='he_normal')(merge7)
+        covn7 = Conv2D(filters=256,
+                       kernel_size=2,
+                       activation='relu',
+                       padding='same',
+                       kernel_initializer='he_normal')(conv7)
+
+        # ---------------------------------------------------------------------
+        # Do Upsampling 
+        up8 = Conv2D(filters=128,
+                     kernel_size=2,
+                     activation='relu',
+                     padding='same',
+                     kernel_initializer='he_normal')(UpSampling2D(size=(2,2))(conv7))
+        
+        
+        # ---------------------------------------------------------------------
+        # Third conv and merge in expanding path
+        merge8 = merge([conv2,up8],
+                       mode='concat',
+                       concat_axis=3)
+        conv8 = Conv2D(filters=128,
+                       kernel_size=2,
+                       activation='relu',
+                       padding='same',
+                       kernel_initializer='he_normal')(merge8)
+        covn8 = Conv2D(filters=128,
+                       kernel_size=2,
+                       activation='relu',
+                       padding='same',
+                       kernel_initializer='he_normal')(conv8)                       
+
+        # ---------------------------------------------------------------------
+        # Do Upsampling 
+        up9 = Conv2D(filters=64,
+                     kernel_size=2,
+                     activation='relu',
+                     padding='same',
+                     kernel_initializer='he_normal')(UpSampling2D(size=(2,2))(conv7))
+        
+        
+        # ---------------------------------------------------------------------
+        # Fourth conv and merge in expanding path
+        merge9 = merge([conv2,up8],
+                       mode='concat',
+                       concat_axis=3)
+        conv9 = Conv2D(filters=64,
+                       kernel_size=2,
+                       activation='relu',
+                       padding='same',
+                       kernel_initializer='he_normal')(merge9)
+        covn9 = Conv2D(filters=64,
+                       kernel_size=2,
+                       activation='relu',
+                       padding='same',
+                       kernel_initializer='he_normal')(conv9)                       
+        conv9 = Conv2D(filters=2,
+                       kernel_size=3,
+                       activation='relu',
+                       padding='same',
+                       kernel_initializer='he_normal')(conv9)
+                       
+
+        # ---------------------------------------------------------------------
+        # Final Output                             
+        conv9 = Conv2D(filters=1,
+                       kernel_size=1,
+                       activation='relu',
+                       padding='same',
+                       kernel_initializer='he_normal')(conv9)
+        # ---------------------------------------------------------------------
+
+        model = Model(input=inpt, output=conv9)
+
+        return model
