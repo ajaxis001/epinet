0860b0460df0bbf46623bb680c46b8ad840e3936
Nambisan <akn36d@mst.edu>
2018-03-08 13:14:09 -0600

    Chucked numericalSort and using order by time of file creation as that should be around the same for the data and corresponding labels batch

--- train_epinet.py
+++ train_epinet.py
@@ -57,15 +57,10 @@ training_batch_label_folder = os.path.join('Train_batches','labels_'+ str(patch_
 
 
 # The batches of training data
-def numericalSort(value):
-    reg_exp = re.compile(r'\d+')
-    parts = reg_exp.split(value)
-    return parts
-
 tr_data_batches = sorted(glob.glob(os.path.join(dirname, training_batch_img_folder, '*.npy')), 
-                         key=numericalSort)
+                         key=os.path.getmtime)
 tr_label_batches = sorted(glob.glob(os.path.join(dirname, training_batch_label_folder, '*.npy')),
-                          key=numericalSort)
+                          key=os.path.getmtime)
 pprint.pprint(tr_data_batches)
 pprint.pprint(tr_label_batches)
 quit()
