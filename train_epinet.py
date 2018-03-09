91010a341162cf97199051563507315070a59295
Nambisan <akn36d@mst.edu>
2018-03-09 15:08:43 -0600

    Trying to change training code so that i dont have to change how the data was processed

--- train_epinet.py
+++ train_epinet.py
@@ -69,7 +69,7 @@ pprint.pprint(tr_label_batches)
 loss='binary_crossentropy'
 optimizer='adam'
 batch_size = 5
-epochs = 200
+epochs = 1 # this is how many times the batch of patches of a single image will be sent through the network
 verbose_in_fit = 1
 early_stopping_patience = 30
 
@@ -94,91 +94,111 @@ number_of_batches = len(tr_data_batches)
 print('number_of_batches : ', number_of_batches)   
 
 mode = 'train'
+
+# init path names to display outside loop
 save_weights_full_path = ''
 log_full_path = ''
-# test training using a single trdata and trlabel npy files 
-for idx in range(number_of_batches): 
-    
-    if idx > 0:
-        load_weights_name =   'model_weights_idx_'+ str(idx-1) +'.h5'
+
+batch_epochs = 30 # Number of times all the image batches in the training data will be seen by the network 
+real_epochs = number_of_batches * 30 # total number of forward and backward passes on a single image (NOTE : not patches of the image) 
+
+for idx_b in range(batch_epochs):
+    print('\n\nBatch Epoch Number : ', str(idx_b) )
+
+    if idx_b > 0: 
+        # If not the first epoch load the weights from the last batch of previous epoch 
+        load_weights_name =   'model_weights_idx_'+ str(number_of_batches-1) + '_bepoch_' + str(idx_b-1)  +'.h5'
         load_weights_full_path = os.path.join(dirname,
                                               models_folder,
                                               suite_dirname,
                                               save_weights_to_path,
                                               load_weights_name)
         epinet.load_weights(load_weights_full_path) 
-        print('Loaded weights from training patch batch : ' + str(idx-1)) 
+        print('Loaded weights from patch batch : ' + str(idx-1)  ' in Training epochs : ' + str(idx_b-1)  ) 
 
-    # Preprocesing / extracting data 
-    train_data_labels = ready_data(tr_data_batches[idx], tr_label_batches[idx], mode)
-    
-    X_data = train_data_labels['X_data']
-    num_of_tr_imgs, img_rows, img_cols, img_channels = X_data.shape
+    # test training using a single trdata and trlabel npy files 
+    for idx in range(number_of_batches): 
+        
+        if idx > 0 :
+            load_weights_name =   'model_weights_idx_'+ str(idx-1) + '_bepoch_' + str(idx_b)  +'.h5'
+            load_weights_full_path = os.path.join(dirname,
+                                                  models_folder,
+                                                  suite_dirname,
+                                                  save_weights_to_path,
+                                                  load_weights_name)
+            epinet.load_weights(load_weights_full_path) 
+            print('Loaded weights from training patch batch : ' + str(idx-1)) 
 
-    y_data = train_data_labels['Y_data'] > 0 # (converting  data  to -> 0 to 1)
-    
-    print('X_data.shape : ' , X_data.shape)
-    print('y_data.shape : ' , y_data.shape)
+        # Preprocesing / extracting data 
+        train_data_labels = ready_data(tr_data_batches[idx], tr_label_batches[idx], mode)
+        
+        X_data = train_data_labels['X_data']
+        num_of_tr_imgs, img_rows, img_cols, img_channels = X_data.shape
 
-    # Splitting training data into training and validation data (stratified cross validation)
-    val_per = 0.20 # ratio of training data to be taken for validation
-    X_train, X_val = train_test_split(X_data,
-                                      random_state=random_state,
-                                      test_size=val_per)
-    y_train, y_val = train_test_split(y_data,
-                                      random_state=random_state,
-                                      test_size=val_per)
-    
-    
-    # Declaring the model
-    my_model = unet_model1(img_rows=img_rows,
-                                 img_cols=img_cols,
-                                 img_channels=img_channels)
-    epinet = my_model.get_model()
+        y_data = train_data_labels['Y_data'] > 0 # (converting  data  to -> 0 to 1)
+        
+        print('X_data.shape : ' , X_data.shape)
+        print('y_data.shape : ' , y_data.shape)
 
-
-    # Compile model
-    epinet.compile(loss=loss,
-                   optimizer='adam',
-                   metrics=['accuracy'])
-
-    # Print the model summary
-    epinet.summary()
-   
-    # Save the model accuracy and loss
-    log_name = 'model_idx_'+ str(idx) +'.log'
-    log_full_path = os.path.join(dirname,
-                             models_folder,
-                             suite_dirname, 
-                             log_path, 
-                             log_name)
-    csv_logger_call = CSVLogger(log_full_path)
+        # Splitting training data into training and validation data (stratified cross validation)
+        val_per = 0.20 # ratio of training data to be taken for validation
+        X_train, X_val = train_test_split(X_data,
+                                          random_state=random_state,
+                                          test_size=val_per)
+        y_train, y_val = train_test_split(y_data,
+                                          random_state=random_state,
+                                          test_size=val_per)
+        
+        
+        # Declaring the model
+        my_model = unet_model1(img_rows=img_rows,
+                                     img_cols=img_cols,
+                                     img_channels=img_channels)
+        epinet = my_model.get_model()
 
 
-    # Save model for testing purposes
-    save_weights_name =   'model_weights_idx_'+ str(idx) +'.h5'
-    save_weights_full_path = os.path.join(dirname,
-                                      models_folder,
-                                      suite_dirname,
-                                      save_weights_to_path,
-                                      save_weights_name)
-    model_chkpt_call = ModelCheckpoint(save_weights_full_path,
-                                       monitor='val_acc',
-                                       save_best_only=True,
-                                       save_weights_only=True)
+        # Compile model
+        epinet.compile(loss=loss,
+                       optimizer='adam',
+                       metrics=['accuracy'])
 
-    # Use early stopping w.r.t val_loss to stop model from overfitting or needlessly training (when bad model)
-    early_stopping = EarlyStopping(monitor='val_loss', 
-                                   patience=early_stopping_patience)
+        # Print the model summary
+        epinet.summary()
+       
+        # Save the model accuracy and loss
+        log_name = 'model_idx_'+ str(idx) + '_bepoch_' + str(idx_b) + '.log'
+        log_full_path = os.path.join(dirname,
+                                 models_folder,
+                                 suite_dirname, 
+                                 log_path, 
+                                 log_name)
+        csv_logger_call = CSVLogger(log_full_path)
 
 
-    # Train model
-    epinet.fit(X_train, y_train,
-                  batch_size=batch_size,
-                  epochs=epochs,
-                  verbose=verbose_in_fit,
-                  callbacks=[csv_logger_call, model_chkpt_call, early_stopping],
-                  validation_data=(X_val, y_val))
+        # Save model for testing purposes
+        save_weights_name =   'model_weights_idx_'+ str(idx)  + '_bepoch_' + str(idx_b) + '.h5'
+        save_weights_full_path = os.path.join(dirname,
+                                          models_folder,
+                                          suite_dirname,
+                                          save_weights_to_path,
+                                          save_weights_name)
+        model_chkpt_call = ModelCheckpoint(save_weights_full_path,
+                                           monitor='val_acc',
+                                           save_best_only=True,
+                                           save_weights_only=True)
+
+        # Use early stopping w.r.t val_loss to stop model from overfitting or needlessly training (when bad model)
+        early_stopping = EarlyStopping(monitor='val_loss', 
+                                       patience=early_stopping_patience)
+
+
+        # Train model
+        epinet.fit(X_train, y_train,
+                      batch_size=batch_size,
+                      epochs=epochs,
+                      verbose=verbose_in_fit,
+                      callbacks=[csv_logger_call, model_chkpt_call, early_stopping],
+                      validation_data=(X_val, y_val))
 
 print(save_weights_full_path)
 print(log_full_path)
