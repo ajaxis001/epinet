c86f47721b9659147e78fce437b21a964cc6294f
Nambisan <akn36d@mst.edu>
2018-03-16 17:07:08 -0500

    Using batch create function v2 to generate training data and validation data to retrain u-net better (TODO: use keras flow_from_directory instead of this method)

--- create_patches.py
+++ create_patches.py
@@ -34,31 +34,41 @@ patch_rows = 256
 patch_cols = 256
 patch_step = 100 # sets the number of pixels between start of one patch and the start of the succeeding patch
 
-number_of_batches = 15
+number_of_batches = 20
+
+# Validation split percentage w.r.t total training data size
+val_per = (20.0/100.0)
 
 # Setting folders to store the batches of .npy files that will be generated
 training_batch_img_folder = os.path.join('Train_batches','images_'+ str(patch_rows) + '_' + str(patch_step) + '_' + str(number_of_batches)) 
 training_batch_label_folder = os.path.join('Train_batches','labels_'+ str(patch_rows) + '_' + str(patch_step) +  '_' + str(number_of_batches)) 
 
+# Setting the validation folders to the corresponing training data and labels folder
+validation_batch_img_folder =  training_batch_img_folder
+validation_batch_label_folder = training_batch_label_folder
+
 # make the folders if they dont exist 
 makefolder_ifnotexists(training_batch_img_folder)
+
 makefolder_ifnotexists(training_batch_label_folder)
 
 
-print('The batches will be stored in the following folders : ')
+print('The batches (training and validation) will be stored in the following folders : ')
 print(training_batch_img_folder)
 print(training_batch_label_folder)
 
-
+# Extension for the images
 img_extension = 'tif'
 
-batch_patchCreateSave(path_raw_img, path_masks,
+batch_patchCreateSave_v2(path_raw_img, path_masks,
                       training_batch_img_folder, training_batch_label_folder, 
+                      validation_batch_img_folder, validation_batch_label_folder, 
                       number_of_batches,
-                      patch_rows, patch_cols, 
-                      patch_step, img_extension)
+                      patch_rows, patch_cols, patch_step, 
+                      img_extension, 
+                      val_per)
         
-print('Stored data and labels in folders : \n' + training_batch_img_folder + '\n' + training_batch_label_folder)    
+print('Stored data and labels (Training and validation) in folders : \n' + training_batch_img_folder + '\n' + training_batch_label_folder)    
         
     
     
