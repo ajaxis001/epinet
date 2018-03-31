24ad2a20d0cf442b43de43d05d796f28671e2509
Anand K Nambisan <arthurpenhaligan@gmail.com>
2018-03-31 02:36:33 -0500

    Trying to use flow_from_directory() to overcome hardware limitations on the forge

--- test_flow_from_dir.py
+++ test_flow_from_dir.py
@@ -7,7 +7,7 @@ import sys
 # import glob2 as glob
 import glob as glob
 
-import pprint
+from pprint import pprint
 import numpy as np
 import matplotlib.pyplot as plt  
 
@@ -15,9 +15,14 @@ from skimage import io
 
 from utilities_episeg import *
 
+dirname = os.path.dirname(__file__)
+
 # Specifying paths (with the epithelium images and the segmentation mask images)
-path_raw_img = os.path.join('Images50','epi_imgs')# the raw images (same resolutiona as the svs)
+path_raw_img = os.path.join('Images50','Images')# the raw images (same resolutiona as the svs)
 path_masks = os.path.join('Images50','Mask') # the segmentation mask
+
+print('path to images : ', path_raw_img)
+print('path to masks : ', path_masks)
 
 # We will create smaller image patches over the image
 patch_rows = 256
@@ -31,55 +36,60 @@ img_extension = 'tif'
 img_files = glob.glob(os.path.join(path_raw_img, '*.*'))
 mask_files = glob.glob(os.path.join(path_masks, '*.*'))
 
-quit()
+pprint(img_files)
+print(30*'=')
+pprint(mask_files)
+
 
 #======================================== Actual Testing of flow_from_directory() ========================================
 
 seed = 1
 
-data_gen_args = dict(rotation_range=90.,
-                     width_shift_range=0.1,
-                     height_shift_range=0.1,
-                     zoom_range=0.2)
-
+data_gen_args = dict()
 image_datagen = ImageDataGenerator(**data_gen_args)
 mask_datagen = ImageDataGenerator(**data_gen_args)
 
+# Defining random_crop() to do random cropping to get patches randomly from data 
+def random_crop(x, random_crop_size, sync_seed=None, **kwargs):
+    np.random.seed(sync_seed)
+    w, h = x.shape[1], x.shape[2]
+    rangew = (w - random_crop_size[0]) // 2
+    rangeh = (h - random_crop_size[1]) // 2
+    offsetw = 0 if rangew == 0 else np.random.randint(rangew)
+    offseth = 0 if rangeh == 0 else np.random.randint(rangeh)
+    return x[:, offsetw:offsetw+random_crop_size[0], offseth:offseth+random_crop_size[1]]
+
+image_datagen.config['random_crop_size'] = (patch_rows, patch_cols)
+mask_datagen.config['random_crop_size'] = (patch_rows, patch_cols)
+
+image_datagen.set_pipeline([random_crop])
+mask_datagen.set_pipeline([random_crop])
+
+
 # -------------------------------- For Mask------------------------------------
 i = 0
-for batch in image_datagen.flow_from_directory(training_batch_img_folder, 
-                                                target_size=(patch_rows,patch_cols),
-                                                batch_size=10,
-                                                class_mode=None,
-                                                save_to_dir='preview_ffd',
-                                                save_prefix='img_',
-                                                save_format='jpeg',
-                                                seed=seed):
+img_flow = image_datagen.flow_from_directory(path_raw_img, 
+		                            target_size=(patch_rows,patch_cols),
+		                            batch_size=10,
+		                            class_mode=None,
+		                            save_to_dir='preview_ffd',
+		                            save_prefix='img_',
+		                            save_format='jpeg',
+		                            seed=seed)
 
-    i+=1
-    if i > 10:
-        break
+x_batch = next(img_flow)
 
 # -------------------------------- For Mask------------------------------------
 i = 0
-for batch in image_datagen.flow_from_directory(training_batch_img_folder, 
-                                                target_size=(patch_rows,patch_cols),
-                                                batch_size=10,
-                                                class_mode=None,
-                                                save_to_dir='preview_ffd',
-                                                save_prefix='mask_',
-                                                save_format='jpeg',
-                                                seed=seed):
-
-    i+=1
-    if i > 10:
-        break
-
-
-
-# combine the two generators into one which yields both image and masks
-train_gen = zip(image_datagen,mask_datagen)
-
+mask_flow = mask_datagen.flow_from_directory(path_masks, 
+	                                target_size=(patch_rows,patch_cols),
+	                                batch_size=10,
+	                                class_mode=None,
+	                                save_to_dir='preview_ffd',
+	                                save_prefix='mask_',
+	                                save_format='jpeg',
+	                                seed=seed)
+y_batch = next(mask_flow)
 
 
 
