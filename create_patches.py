213a355794ad3e1be0b1e3b1f28ada4351ee72ae
Anand K Nambisan <arthurpenhaligan@gmail.com>
2018-04-05 21:19:54 -0500

    Repurposed the create_patches.py just to create validation data batch

--- create_patches.py
+++ create_patches.py
@@ -25,8 +25,8 @@ from skimage import io
 from utilities_episeg import *
 
 # Specifying paths (with the epithelium images and the segmentation mask images)
-path_raw_img = os.path.join('Images50','epi_imgs')# the raw images (same resolutiona as the svs)
-path_masks = os.path.join('Images50','Mask') # the segmentation mask
+path_raw_img = os.path.join('Images50','img_val')# the raw images (same resolutiona as the svs)
+path_masks = os.path.join('Images50','mask_val') # the segmentation mask
 
 
 # We will create smaller image patches over the image
@@ -34,31 +34,28 @@ patch_rows = 256
 patch_cols = 256
 patch_step = 100 # sets the number of pixels between start of one patch and the start of the succeeding patch
 
-number_of_batches = 20
+number_of_batches = 0
 
-# Validation split percentage w.r.t total training data size
-val_per = (20.0/100.0)
+val_per = 100
 
 # Setting folders to store the batches of .npy files that will be generated
-training_batch_img_folder = os.path.join('Train_batches','images_'+ str(patch_rows) + '_' + str(patch_step) + '_' + str(number_of_batches)) 
-training_batch_label_folder = os.path.join('Train_batches','labels_'+ str(patch_rows) + '_' + str(patch_step) +  '_' + str(number_of_batches)) 
-
-# Setting the validation folders to the corresponing training data and labels folder
-validation_batch_img_folder =  training_batch_img_folder
-validation_batch_label_folder = training_batch_label_folder
+validation_batch_img_folder = os.path.join('Val_batches','images_'+ str(patch_rows) + '_' + str(patch_step) )
+validation_batch_label_folder = os.path.join('Val_batches','labels_'+ str(patch_rows) + '_' + str(patch_step)) 
 
 # make the folders if they dont exist 
-makefolder_ifnotexists(training_batch_img_folder)
+makefolder_ifnotexists(validation_batch_img_folder)
+makefolder_ifnotexists(validation_batch_label_folder)
 
-makefolder_ifnotexists(training_batch_label_folder)
 
-
-print('The batches (training and validation) will be stored in the following folders : ')
-print(training_batch_img_folder)
-print(training_batch_label_folder)
+print('The batches (validation) will be stored in the following folders : ')
+print(validation_batch_img_folder)
+print(validation_batch_label_folder)
 
 # Extension for the images
 img_extension = 'tif'
+
+training_batch_img_folder = validation_batch_img_folder
+training_batch_label_folder = validation_batch_label_folder
 
 batch_patchCreateSave_v2(path_raw_img, path_masks,
                       training_batch_img_folder, training_batch_label_folder, 
@@ -68,7 +65,7 @@ batch_patchCreateSave_v2(path_raw_img, path_masks,
                       img_extension, 
                       val_per)
         
-print('Stored data and labels (Training and validation) in folders : \n' + training_batch_img_folder + '\n' + training_batch_label_folder)    
+print('Stored data and labels (validation) in folders : \n' + validation_batch_img_folder + '\n' + validation_batch_label_folder)    
         
     
     
